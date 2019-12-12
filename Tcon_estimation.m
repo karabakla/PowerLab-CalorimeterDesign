66c416eb490cd6fa20ec3d088a2193f22dca8421
Emre KARABAKLA <karabakla60@gmail.com>
2019-12-12 22:08:58 +0300

    Matlab files for calculation

---
+++ Tcon_estimation.m
@@ -0,0 +1,34 @@
+﻿data=xlsread('20wData.xlsx');
+%data=xlsread('50wData.xlsx');
+%data=xlsread('40w_data.csv');
+time=data(:,1)-5;
+T=data(:,2);
+
+L=0.45;%length in meters
+W=0.35;%width in meters
+H=0.2;%height in meters
+Tamb=T(1);%Ambient temp. in Kelvin
+Tin0=T(1);%Initial temp. inside box in Kelvin
+dwall=7;%wall thickness in cm
+Tcon=0.075;%Spesific thermal conductivity of polystyrene in Watts/(Kelvin*meters)  
+%Cp=210000;%Spesific constant volume heat capacity j/(kg*K)
+Cp=1000;
+%time=1:1:101;%working time in minutes
+
+%Ploss=Vol*(1.187-(0.175/52)*T)dT/dt+sum((T-Tamb)Tcon*A/dwall)
+
+%Ploss*t=Vol(1.187(T-Tin0)-(0.175/104)*T^2)+sum((T-Tamb)*Tcon*A/dwall*t)
+
+%Calculations
+Vol=L*W*H;
+
+A1=2*H*L;  %front and back face area
+A2=2*H*W;  %left and right face area
+A3=2*L*W;  %top and bottom face area
+
+G1th=100*A1/dwall;%constant for face A1&A2
+G2th=100*A2/dwall;%constant for face A1&A2
+G3th=100*A3/dwall;%constant for face A1&A2
+Ploss=20;
+Tcon= Ploss/((T(length(T))-Tamb)*(G1th+G2th+G3th))
+
